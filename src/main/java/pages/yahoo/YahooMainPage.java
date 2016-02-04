963e6fc28f8dbd1386a17aa29c0cd96783fabe41
ikorol <P@ssword123>
2016-02-05 01:21:30 +0200

    passed for Yahoo for hidden links

--- src/main/java/pages/yahoo/YahooMainPage.java
+++ src/main/java/pages/yahoo/YahooMainPage.java
@@ -4,6 +4,7 @@ import config.ConfigProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +14,17 @@ import java.util.List;
 public class YahooMainPage extends BasePage {
     private static final Logger logger = LoggerFactory.getLogger(YahooMainPage.class);
     private final String MENU_ITEM_LOCATOR = "//*[@id='Navigation']//span[contains(.,'%s')]";
+    private final String SEE_MORE_MENU_ITEM_LOCATOR = "//*[contains(@class,'js-navlinks-seemore-menu')]//span[contains(.,'%s')]";
 
     @FindBy(css = ".navlist li[style=''] span")
     List<WebElement> sitesUrls;
+
+    @FindBy(css = ".js-navlinks-seemore-menu span")
+    List<WebElement> seeMoreMenu;
+
+
+    @FindBy(css = "[aria-label='More Navlinks']")
+    WebElement moreYahooLink;
 
     public YahooMainPage(WebDriver driver) {
         super(driver);
@@ -24,22 +33,35 @@ public class YahooMainPage extends BasePage {
     public List<WebElement> getSiteLinks(){
         return sitesUrls;
     }
+    public List<WebElement> getMoreSiteLinks(){
+        return seeMoreMenu;
+    }
 
     public void open(String url){
         driver.get(url);
     }
     public void open(){
-        driver.get(ConfigProperties.YAHOO_BASE_URL);
+        open(ConfigProperties.YAHOO_BASE_URL);
     }
 
     private WebElement getNavigationMenuItem(String menuName) {
         return driver.findElement(By.xpath(String.format(MENU_ITEM_LOCATOR, menuName)));
     }
 
+    private WebElement getSeeMoreMenuItem(String menuName) {
+        return driver.findElement(By.xpath(String.format(SEE_MORE_MENU_ITEM_LOCATOR, menuName)));
+    }
+
     public void clickNavigationMenuItem(String menuName) {
-        logger.info("Click menu item '{}'", menuName);
-        getNavigationMenuItem(menuName).click();
+        if(!getNavigationMenuItem(menuName).isDisplayed()){
+            logger.info("Click '...More' to see hidden menu items");
+            Actions action = new Actions(driver);
+            action.moveToElement(moreYahooLink).build().perform();
+            getSeeMoreMenuItem(menuName).click();
+        }else{
+            getNavigationMenuItem(menuName).click();
+        }
+        logger.info("Menu item '{}' was been clicked", menuName);
     }
 
 }
-
--- src/test/java/tests/YahooTest.java
+++ src/test/java/tests/YahooTest.java
@@ -1,21 +1,20 @@
 package tests;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.yahoo.YahooMainPage;
 import utils.WebDriverFactory;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
@@ -23,46 +22,59 @@ import static org.junit.Assert.assertTrue;
 @RunWith(value = Parameterized.class)
 public class YahooTest {
     private static final Logger logger = LoggerFactory.getLogger(YahooTest.class);
-    private WebDriver driver;
-    private String menuItem= "Mail";
+    private static WebDriver driver;
+    private String menuItem = "Mail";
     private static List<WebElement> menuItems;
     private YahooMainPage yahooPage;
+    private long timeout;
 
-    public YahooTest(String value) {
+    public YahooTest(String value, long timeout) {
         this.menuItem = value;
+        this.timeout = timeout;
+    }
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = WebDriverFactory.getFirefoxDriver();
+
     }
 
     @Before
-    public void openBrowser(){
-        driver = WebDriverFactory.getFirefoxDriver();
+    public void clearCookie(){
+        driver.manage().deleteAllCookies();
         yahooPage = new YahooMainPage(driver);
     }
 
-    @After
-    public void closeBrowser(){
+    @AfterClass
+    public static void closeBrowser(){
         driver.close();
     }
 
-    @Parameters(name = "{index}: menu is {0}")
+    @Parameters(name = "{index}: Yahoo {0}")
     public static Iterable<Object[]> data() {
-        HtmlUnitDriver driverLocal = new HtmlUnitDriver();
-        YahooMainPage yahoo = new YahooMainPage(driverLocal);
-        yahoo.open();
-        menuItems = yahoo.getSiteLinks();
-        Collection<Object[]> result = new ArrayList<Object[]>();
-        for (WebElement e: menuItems){
-            String link = e.getText();
-            logger.info("Yahoo menu item is {}", link);
-            result.add(new Object[]{link});
-            break;
-        }
-        driverLocal.quit();
-        return result;
+//        HtmlUnitDriver driverLocal = new HtmlUnitDriver();
+//        YahooMainPage yahoo = new YahooMainPage(driverLocal);
+//        yahoo.open();
+//        menuItems = yahoo.getSiteLinks();
+//        assertTrue("Unable get Yahoo left-side menu items", menuItems.size()>0);
+//        Collection<Object[]> result = new ArrayList<Object[]>();
+//        for (WebElement e: menuItems){
+//            String link = e.getText();
+//            logger.info("Yahoo menu item is {}", link);
+//            result.add(new Object[]{link});
+//        }
+//        driverLocal.quit();
+
+        return Arrays.asList(new Object[][]{
+               // {"Mail"},{"News"},{"Sports"},{"Finance"},{"Mail"}
+//                {"Mail", 15, true},{"Mail", 6, true},{"Mail",8, true},{"Mail", 4, true},{"Mail", 15, true}
+                {"Makers", 15},{"Mail", 15}
+        });
     }
 
     @Test
     public void verifyTimeOfLoadingYahooSitesTest()  {
-        long expLoadPageTime = 7;
+        long expLoadPageTime = timeout;
         String message = "'Yahoo %s' loading over %s sec";
         yahooPage.open();
         yahooPage.clickNavigationMenuItem(menuItem);
